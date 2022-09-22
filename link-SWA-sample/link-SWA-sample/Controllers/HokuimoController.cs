534593a4e61666380fac1d2c77c1312abc18d79c
ry-suzuki <ry-suzuki@kentem.co.jp>
2022-09-22 13:56:29 +0900

    first commit

---
+++ link-SWA-sample/link-SWA-sample/Controllers/HokuimoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace link_SWA_sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HokuimoController : ControllerBase
+    {
+        private readonly ILogger<HokuimoController> _logger;
+
+        public HokuimoController(ILogger<HokuimoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetHokuimo")]
+        public string Get()
+        {
+            return DoHokuimo();
+        }
+
+        private string DoHokuimo()
+        {
+            var hkim = "ホクイモ".OrderBy(x => Guid.NewGuid()).Take(2);
+            var hk = string.Join("", hkim);
+            var im = string.Join("", hkim);
+            return $"{hk}{hk}の{im}";
+        }
+    }
+}
